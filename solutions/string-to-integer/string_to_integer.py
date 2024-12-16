b82e9c440c5e61b157c7752ec8bd299b90f9621a
halfrice <halfrice@protonmail.com>
2024-12-16 15:31:38 -0800

    8. String to Integer (atoi)

---
+++ solutions/string-to-integer/string_to_integer.py
@@ -0,0 +1,44 @@
+# string_to_integer.py
+
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        if not s:
+            return 0
+
+        # Strip whitespace
+        s = s.lstrip()
+
+        # Determine sign
+        is_negative = False
+        i = 0
+        s_len = len(s)
+        if i < s_len and s[i] == '-':
+            is_negative = True
+            i += 1
+        elif i < s_len and s[i] == '+':
+            i += 1
+
+        # Read ints until first non-int is found
+        r = ''
+        while i < s_len and s[i].isdigit():
+            r += s[i]
+            i += 1
+        if not r:
+            return 0
+
+        # Restore sign
+        if is_negative:
+            r = '-' + r
+
+        # Cast string to int
+        r = int(r)
+
+        # Round ints that are out of 32-bit signed int bounds
+        negative_limit = -(2**31)
+        positive_limit = 2**31 - 1
+        if r < negative_limit:
+            return negative_limit
+        elif r > positive_limit:
+            return positive_limit
+        return r
