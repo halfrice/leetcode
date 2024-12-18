4e8115920626d93f941aaef309bb282bc1238cc1
halfrice <halfrice@protonmail.com>
2024-12-17 16:05:26 -0800

    10. Regular Expression Matching

---
+++ solutions/regular-expression-matching/test_regular_expression_matching.py
@@ -0,0 +1,40 @@
+# test_regular_expression_matching.py
+
+from regular_expression_matching import Solution
+
+solution = Solution()
+
+
+def test_case_1():
+    s = 'aa'
+    p = 'a'
+    expected = False
+    assert solution.isMatch(s, p) == expected
+
+
+def test_case_2():
+    s = 'aa'
+    p = 'a*'
+    expected = True
+    assert solution.isMatch(s, p) == expected
+
+
+def test_case_3():
+    s = 'ab'
+    p = '.*'
+    expected = True
+    assert solution.isMatch(s, p) == expected
+
+
+def test_custom_case_1():
+    s = 'zyzz'
+    p = 'x*..z*'
+    expected = True
+    assert solution.isMatch(s, p) == expected
+
+
+def test_custom_case_2():
+    s = ''
+    p = ''
+    expected = True
+    assert solution.isMatch(s, p) == expected
